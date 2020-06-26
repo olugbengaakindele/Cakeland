56592420b515cac84e0d6881ba8f35991a1a4ff9
olugbenga Akindele <akindelegbenga@gmail.com>
2020-06-25 22:18:45 -0400

    all routes added

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -8,6 +8,26 @@ from flask import render_template, url_for, redirect
 def index():
     return render_template('index.html', title='Landing_Page')
 
+
 @auth.route("/home")
 def home():
     return render_template('home.html', title='Home')
+
+
+@auth.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@auth.route("/gallery")
+def gallery():
+    return render_template('gallery.html', title='gallery')
+
+
+@auth.route("/pricelist")
+def pricelist():
+    return render_template('pricelist.html', title='pricelist')
+
+
+@auth.route("/contactus")
+def contactus():
+    return render_template('contactus.html', title='contactus')
--- app/static/js/ui.js
+++ app/static/js/ui.js
@@ -1,1 +1,1 @@
-document.querySelector(".h5").style.color = "#fee0000";
+document.querySelector(".h1").style.color = "red";
